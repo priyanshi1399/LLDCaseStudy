66ce7c716f4f8f857fea8e841ef5e26e4c5cfce6
priyanshi1399 <priyanshitomar001@gmail.com>
2025-10-07 23:09:20 +0530

    SnakeAndLadder

---
+++ src/SnakeAndLadder/Cell.java
@@ -0,0 +1,14 @@
+package SnakeAndLadder;
+
+public class Cell {
+
+    Jump jump;
+
+    public Jump getJump() {
+        return jump;
+    }
+
+    public void setJump(Jump jump) {
+        this.jump = jump;
+    }
+}
